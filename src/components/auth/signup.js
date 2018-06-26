27e3b2cd44e3337d7d23c67175df77066463674d
Joshua Harris <jnharris208@gmail.com>
2018-06-26 15:43:29 -0600

    setup auth routes and headerwrapper component

---
+++ src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Signup extends Component {
+    render() {
+        return (
+            <div className='sign-un'>
+                ...signup
+            </div>
+        )
+    }
+}
+
+export default Signup;
